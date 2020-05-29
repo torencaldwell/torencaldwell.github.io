c13a406a90a6bbccea6b21dcaf0028195d247dd3
torencaldwell <torencaldwell@gmail.com>
2020-05-29 16:32:48 -0500

    added parallax effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Parallax, Background } from 'react-parallax';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This page is under construction ;)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={300} style={{ width: '100%' }} bgStyle={{ width: '100%' }}>
+        <Background style={{width: '100%'}}>
+          <div style={{
+            width: '100%',
+            height: '400px',
+            backgroundColor:'#666666'
+          }}>
+            <img src={require('./assets/background.jpg')}/>
+            <div style={{height: '200px'}}/>
+            <h1 style={{textAlign: 'center'}}>This is a text..</h1>
+          </div>
+        </Background>
+
+        <div style={{height: '500px'}}>
+          <p>This is text</p>
+        </div>
+      </Parallax>
+      <div style={{ height:'5000px' }} />
     </div>
   );
 }
