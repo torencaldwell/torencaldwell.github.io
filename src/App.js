e8a4ac2d50da10a22fd468ae3cb765655d6f6c07
torencaldwell <torencaldwell@gmail.com>
2020-06-18 23:06:52 -0500

    added some sample text to body

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ const App = () => {
         backgroundColor: colors.midnightBlue,
         position: 'relative',
         zIndex: 2
-      }} />
+      }}>
+        <h1>Buncha Conent's gonna go here</h1>
+        <p>A whole lot of it. promise. gonna be huge. big ol' deal. you'll see.</p>
+      </div>
     </div> 
   );
 }
